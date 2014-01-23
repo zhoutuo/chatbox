fbe0dd960a1c94738e328265fe804dc48b2467ce
Zhoutuo Yang <zhoutuoy@gmail.com>
2014-01-23 11:26:33 -0800

    Integrates WTForm with Tornado for Registration form

--- handlers.py
+++ handlers.py
@@ -1,5 +1,30 @@
+import datetime
 import tornado.web
+import forms
+import ext
 
-class RegisterHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+	def get_current_user(self):
+		return self.get_secure_cookie("user")
+
+
+class RoomHandler(BaseHandler):
 	def get(self):
-		self.render('template/register.html')
+		if not self.get_current_user:
+			self.redirect("/register")
+		else:
+			pass
+
+
+class RegisterHandler(BaseHandler):
+	def get(self):
+		form = forms.RegistrationForm()
+		self.render('register.html', form=form)
+
+	def post(self):
+		form = forms.RegistrationForm(ext.TornadoMultiDict(self.request.arguments))
+		if form.validate():
+			self.write("Conguratulations")
+		else:
+			self.render('register.html', form=form)
+
