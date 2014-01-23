5d36a40f00d8f6d23e8c803c95a7492b9167a647
Zhoutuo Yang <zhoutuoy@gmail.com>
2014-01-23 11:24:47 -0800

    Updates application settings

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
 import tornado.ioloop
-from tornado import autoreload
 import handlers
 
 if __name__ == "__main__":
 	settings = {
-		"static_path": os.path.join(os.path.dirname(__file__), "static")
+		"debug": True,
+		"static_path": os.path.join(os.path.dirname(__file__), "static"),
+		"template_path": os.path.join(os.path.dirname(__file__), "template"),
+		"autoescape": None,
+		"cookie_secret": "yJPNtnEgCwVd9PwutDj8"
 	}
 	app = tornado.web.Application([
-		(r'/', handlers.RegisterHandler),
+		(r'/', handlers.RoomHandler),
+		(r'/register', handlers.RegisterHandler),
 	], **settings)
 	app.listen(8888)
 	# auto reload when files get updated
-	loop = tornado.ioloop.IOLoop.instance()
-	autoreload.start(loop)
-	loop.start()
+	tornado.ioloop.IOLoop.instance().start()
