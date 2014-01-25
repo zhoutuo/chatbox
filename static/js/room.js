9dc5ac0198e92686d23bab20e18ee4641c830048
Zhoutuo Yang <zhoutuoy@gmail.com>
2014-01-25 14:27:48 -0800

    Add basic front end files for room page.

--- static/js/room.js
+++ static/js/room.js
@@ -1,10 +1,36 @@
 $(document).ready(function() {
 	var ws = new WebSocket("ws://localhost:8888/chat");
+	var msg_input = $("#msg_input");
+	var msg_output = $("#msg_output");
 	ws.onopen = function() {
-		ws.send("Hello World");
+		// after sucessfully connection, enable message input for users
+		msg_input.prop('disabled', false);
 	};
 
 	ws.onmessage = function(evt) {
-		alert(evt.data);
+		var msg = JSON.parse(evt.data);
+		msg.date = new Date(msg.timestamp);
+		// add message to output area
+		msg_output.val(msg_output.val() + "\n" + msg.date.toString() + "\n" + msg.content);
 	}
+	// simulating summit event, 'enter' key press
+	msg_input.keypress(function(e) {
+		// 13 is the key code for return
+		if (e.which == 13) {
+			if(msg_input.val() === "") {
+				alert("No Empty Message");
+			} else {
+				// contrust a message object
+				var message = {};
+				message.content = msg_input.val();
+				message.timestamp = Date.now();
+				// send in a form of JSON
+				ws.send(JSON.stringify(message));
+				// clear the input
+				msg_input.val("");
+			}
+			// prevent form from submitting
+			e.preventDefault();
+		}
+	});
 });
