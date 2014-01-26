ac1619e811f9e92b8591886bd581ff98ec92457d
Zhoutuo Yang <zhoutuoy@gmail.com>
2014-01-26 13:48:55 -0800

    Add user list panel to front end.

--- static/js/room.js
+++ static/js/room.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var chat_panel = $("#chat_panel");
+	var user_list = $("#user_list");
 
 	var ws = new WebSocket("ws://127.0.0.1:8888/chat");
 	var msg_input = $("#msg_input");
@@ -10,9 +12,28 @@ $(document).ready(function() {
 
 	ws.onmessage = function(evt) {
 		var msg = JSON.parse(evt.data);
+		console.log(msg);
 		msg.date = new Date(msg.timestamp);
-		// add message to output area
-		msg_output.val(msg_output.val() + "\n" + msg.date.toString() + "\n" + msg.content);
+		// construct a date string to display
+		var dateString = "";
+		dateString += (msg.date.getMonth() + 1) + "/" + msg.date.getDate() + " ";
+		dateString += msg.date.getHours() + ":" + msg.date.getMinutes();
+		// type checking
+		if (msg.type == 0) {
+			// join
+			add_user(msg.user_id, msg.content);
+		} else if (msg.type == 1) {
+			// chat
+			// add message to output area
+			msg_output.val(msg_output.val() + "\n" + dateString + "\n" + msg.content);			
+		} else if (msg.type == 2) {
+			// leave
+		} else {
+			// exist
+			for (var i = msg.content.length - 1; i >= 0; i--) {
+				add_user(msg.user_id[i], msg.content[i]);
+			}
+		}
 	}
 	// simulating summit event, 'enter' key press
 	msg_input.keypress(function(e) {
@@ -34,4 +55,20 @@ $(document).ready(function() {
 			e.preventDefault();
 		}
 	});
+
+	var users_pool = {};
+	function add_user(id, user) {
+		if (id in users_pool) {
+			// do nothing if already exist
+		} else {
+			// add to pool
+			users_pool[id] = user;
+			// add UI
+			var div = $("<div/>");
+			div.addClass("user");
+			div.html(user.name);
+			user_list.append(div);
+
+		}
+	}
 });
