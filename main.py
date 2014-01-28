c1d3167f15bec5b755191679b1b0a4904722f4d5
Zhoutuo Yang <zhoutuoy@gmail.com>
2014-01-27 16:55:42 -0800

    Correct a misplaced comment.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import handlers
 
 if __name__ == "__main__":
     settings = {
-        "debug": True,
+        "debug": True,  # auto reload when files get updated
         "static_path": os.path.join(os.path.dirname(__file__), "static"),
         "template_path": os.path.join(os.path.dirname(__file__), "template"),
         "login_url": "/register",
@@ -17,5 +17,4 @@ if __name__ == "__main__":
         (r'/chat', handlers.ChatHandler),
     ], **settings)
     app.listen(8888)
-    # auto reload when files get updated
     tornado.ioloop.IOLoop.instance().start()
